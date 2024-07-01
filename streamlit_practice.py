2d53c3707b4f4bd874334b87176d55d3cea3a303
Midlaj <abdullamidlaj23@gmail.com>
2024-07-01 13:57:52 +0530

    iris added

---
+++ streamlit_practice.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="iris",
+                   page_icon="🌸")
+st.title('Iris')
+
